3b981a552da23d4fbaf8e7ddbe718d9700259344
Elaine <ljc391@nyu.edu>
2016-07-14 12:38:40 -0400

    viewport
    img max-width

--- _headCommon.php
+++ _headCommon.php
@@ -1,5 +1,5 @@
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Raleway" />
     <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet"href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="./styles/main.css" />
-    
